879184744a4950c5fceb1b9c1a8079d4f390d8e9
Skryre <zbk1998@ymail.com>
2022-11-27 17:26:42 -0500

    Refactor the code to have more modules and better code with the function checkWin

--- script.js
+++ script.js
@@ -1,63 +1,74 @@
 const contain = document.querySelectorAll(".key");
 const title = document.querySelector("h3");
-const reset = document.querySelector(".button")
+const reset = document.querySelector("button")
+const boardGrid = document.querySelector(".board")
 
 const settings = (() => {
 
-    create = [1,2,3,4,5,6,7,8,9];
+    board = Array(9).fill('');
     let currentPlayer = 'X';
     let round = 0;
 
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+    function checkWin (currentPlayer) {
+        return winConditions.some(combination => {
+            return combination.every(index => {
+            return board[index] === currentPlayer
+            })
+        })}
+
+    return {board, currentPlayer, round, checkWin};
+
+})();
+
+const gameController = (() => {
+
+    contain.forEach((element) =>
+    element.addEventListener("click", (e) => {
+        if (e.target.textContent !== "") return;
+            settings.board[e.target.dataset.index] = settings.currentPlayer;
+            element.innerHTML = settings.currentPlayer
+            settings.round++
+        if(settings.round === 9){
+            return title.innerHTML = "Draw!!"
+        }
+        else if(settings.checkWin(settings.currentPlayer)){
+            return title.innerHTML = "WINNNNNNNN!!"
+        }
+        else{
+            changeState () 
+        }
+    }));
+
+    function changeState () {
+        settings.currentPlayer = settings.currentPlayer === 'X' ? 'O' : 'X'
+    }
+
+})();
+
+const display = (() =>{
     reset.addEventListener(`click`, () => {
+        resetButton()
+    });
+
+    function resetButton (){
         contain.forEach( function(element){
             element.innerHTML = '';
         });
-        settings.create = [1,2,3,4,5,6,7,8,9];
+        boardGrid.classList.remove('x')
+        settings.board = Array(9).fill('');
         settings.currentPlayer = 'X';
         settings.round = 0;
-        title.innerHTML = "Player X's turn"
-    });
-
-    return {create, currentPlayer, round}
-})();
-
-const player = (type, score ) => {
-    return {type, score}
-}
-
-gameController = (() => {
-    
-    
-    
-    return {}
-})
-
-contain.forEach( function(element){
-    element.addEventListener(`click`, (e) => {     
-        if (element.innerHTML === 'X' || element.innerHTML === 'O'){return}
-        settings.create[e.target.dataset.index] = settings.currentPlayer;
-        element.innerHTML = settings.currentPlayer
-        settings.round++;
-        if (
-            (settings.create[0] === settings.create[1] && settings.create[1] === settings.create[2])||
-            (settings.create[3] === settings.create[4] && settings.create[4] === settings.create[5])||
-            (settings.create[6] === settings.create[7] && settings.create[7] === settings.create[8])||
-            (settings.create[0] === settings.create[3] && settings.create[3] === settings.create[6])||
-            (settings.create[1] === settings.create[4] && settings.create[4] === settings.create[7])||
-            (settings.create[2] === settings.create[5] && settings.create[5] === settings.create[8])||
-            (settings.create[0] === settings.create[4] && settings.create[4] === settings.create[8])||
-            (settings.create[2] === settings.create[4] && settings.create[4] === settings.create[6])
-            ){
-                title.innerHTML=settings.currentPlayer+" Player won";
-            }
-        else if (settings.round ===9 ) {
-            title.innerHTML="It is a Draw!"
-        }
-        else {
-        settings.currentPlayer = settings.currentPlayer === 'X' ? 'O' : 'X';
-        console.log(settings.round, e.target.dataset.index, settings.create)
-        title.innerHTML = "Player "+settings.currentPlayer+"'s turn"
-        }
-    });
-});
-
+        title.innerHTML = "Player X's turn";
+    }
+})()
